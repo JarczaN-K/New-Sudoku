fdc6bda376bbcd5aa7172f54d62a06b1c7abfe95
JarczaN-K <Lothar55D120>
2020-08-10 13:41:59 +0200

    first letters

--- BotProgram/src/main/java/Main.java
+++ BotProgram/src/main/java/Main.java
@@ -1,9 +1,36 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, AWTException {
         String name = "notepad.exe";
         Runtime runtime = Runtime.getRuntime();
         runtime.exec(name);
+        Robot robot = new Robot();
+        robot.setAutoDelay(100);
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_W);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_I);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_A);
+        robot.keyPress(KeyEvent.VK_J);
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_1);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyPress(KeyEvent.VK_J);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_M);
+        robot.keyPress(KeyEvent.VK_SPACE);
+        robot.keyPress(KeyEvent.VK_B);
+        robot.keyPress(KeyEvent.VK_O);
+        robot.keyPress(KeyEvent.VK_T);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_M);
     }
 }
